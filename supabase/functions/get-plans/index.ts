c22b294665b5e662995d659745a876d957875165
Michael Shimeles <michaelwasihun96@gmail.com>
2025-03-03 12:21:36 -0500

    fix edge functions

--- supabase/functions/get-plans/index.ts
+++ supabase/functions/get-plans/index.ts
@@ -1,11 +1,15 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from "https://esm.sh/stripe@13.6.0?target=deno";
-import { corsHeaders } from "../_shared/cors";
 
 const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
     apiVersion: '2023-10-16',
     httpClient: Stripe.createFetchHttpClient(),
 });
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  }
 
 
 serve(async (req) => {
--- supabase/functions/payments-webhook/index.ts
+++ supabase/functions/payments-webhook/index.ts
@@ -1,7 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from "https://esm.sh/stripe@13.6.0?target=deno";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { corsHeaders } from "../_shared/cors";
 
 // Types
 type WebhookEvent = {
@@ -36,6 +35,11 @@ const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
   apiVersion: '2023-10-16',
   httpClient: Stripe.createFetchHttpClient(),
 });
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
 
 // Utility functions
 async function logAndStoreWebhookEvent(
