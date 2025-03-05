4e4b322f456399e3fbe71395006bef2eb938bf5d
Michael Shimeles <michaelwasihun96@gmail.com>
2025-03-05 17:47:42 -0500

    add supabase-functions- back

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -83,7 +83,7 @@ export default function LandingPage() {
   const fetchPlans = async () => {  
     try {
       // Use the Supabase client to call the Edge Function
-      const { data, error } = await supabase.functions.invoke('get-plans');
+      const { data, error } = await supabase.functions.invoke('supabase-functions-get-plans');
       
       if (error) {
         throw error;
@@ -115,7 +115,7 @@ export default function LandingPage() {
     setError("");
     
     try {
-      const { data, error } = await supabase.functions.invoke('create-checkout', {
+      const { data, error } = await supabase.functions.invoke('supabase-functions-create-checkout', {
         body: {
           price_id: priceId,
           user_id: user.id,
